2f8d390701d3d1f110b09706e8b3a473d3e4108d
riyadhossen74 <riyadhossen696969@gmail.com>
2025-11-27 16:34:23 +0600

    added env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwLy_LxUAB-qsGBvGz_Rh1xTGsN86bOEY",
-  authDomain: "dragon-news-85aad.firebaseapp.com",
-  projectId: "dragon-news-85aad",
-  storageBucket: "dragon-news-85aad.firebasestorage.app",
-  messagingSenderId: "76965490229",
-  appId: "1:76965490229:web:bbfe952998bfe2e6d8cb7c",
-  measurementId: "G-PXBDRPGD0E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Optional: log to check if variables are loaded correctly
